f285394cdce2c3120db5778583467e1f75500701
richardpb <rpbeavis@gmail.com>
2022-04-16 00:34:56 +0900

    fix: correct weather image for corrosponding weather type

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -6,11 +6,12 @@
     <div class="row">
       <div class="col-md-4">
         <div class="card">
-          <img
-            src=".././assets/Clear.gif"
-            class="card-img-top"
-            alt=""
-          />
+          <img v-if="weatherData.day0.type === 'Clear'" src=".././assets/Clear.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day0.type === 'Clouds'" src=".././assets/Clouds.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day0.type === 'Drizzle'" src=".././assets/Drizzle.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day0.type === 'Rain'" src=".././assets/Rain.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day0.type === 'Snow'" src=".././assets/Snow.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day0.type === 'Thunderstorm'" src=".././assets/Thunderstorm.gif" class="card-img-top" alt="" />
           <div class="card-body">
             <p>{{ weatherData.day0.date }}</p>
             <h3 class="card-title">{{ weatherData.day0.type }}</h3>
@@ -24,11 +25,12 @@
 
       <div class="col-md-4">
         <div class="card">
-          <img
-            src=".././assets/Clear.gif"
-            class="card-img-top"
-            alt=""
-          />
+          <img v-if="weatherData.day1.type === 'Clear'" src=".././assets/Clear.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day1.type === 'Clouds'" src=".././assets/Clouds.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day1.type === 'Drizzle'" src=".././assets/Drizzle.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day1.type === 'Rain'" src=".././assets/Rain.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day1.type === 'Snow'" src=".././assets/Snow.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day1.type === 'Thunderstorm'" src=".././assets/Thunderstorm.gif" class="card-img-top" alt="" />
           <div class="card-body">
             <p>{{ weatherData.day1.date }}</p>
             <h3 class="card-title">{{ weatherData.day1.type }}</h3>
@@ -42,11 +44,12 @@
 
       <div class="col-md-4">
         <div class="card">
-          <img
-            src="https://cdn.dribbble.com/users/2120934/screenshots/6193524/media/0e653e48615e57898805fd78918e697d.gif"
-            class="card-img-top"
-            alt=""
-          />
+          <img v-if="weatherData.day2.type === 'Clear'" src=".././assets/Clear.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day2.type === 'Clouds'" src=".././assets/Clouds.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day2.type === 'Drizzle'" src=".././assets/Drizzle.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day2.type === 'Rain'" src=".././assets/Rain.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day2.type === 'Snow'" src=".././assets/Snow.gif" class="card-img-top" alt="" />
+          <img v-if="weatherData.day2.type === 'Thunderstorm'" src=".././assets/Thunderstorm.gif" class="card-img-top" alt="" />
           <div class="card-body">
             <p>{{ weatherData.day2.date }}</p>
             <h3 class="card-title">{{ weatherData.day0.type }}</h3>
@@ -67,11 +70,9 @@ export default {
 
   props: ["weatherData"],
 
-  data: () => ({
-    weather: {},
-  }),
+  data: () => ({}),
 
-  methods: {},
+  methods: {},    
 };
 </script>
 
