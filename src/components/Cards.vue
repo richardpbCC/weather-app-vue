1dc55d26ea90631a91251d2620248f9ea708a1c2
richardpb <rpbeavis@gmail.com>
2022-04-15 23:57:55 +0900

    add: assets for weather icon

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -4,11 +4,10 @@
       <h4 class="display-7 fw-bold">3-day forecast</h4>
     </div>
     <div class="row">
-      
       <div class="col-md-4">
         <div class="card">
           <img
-            src="https://cdn.dribbble.com/users/2120934/screenshots/6193524/media/0e653e48615e57898805fd78918e697d.gif"
+            src=".././assets/Clear.gif"
             class="card-img-top"
             alt=""
           />
@@ -26,7 +25,7 @@
       <div class="col-md-4">
         <div class="card">
           <img
-            src="https://cdn.dribbble.com/users/2120934/screenshots/6193524/media/0e653e48615e57898805fd78918e697d.gif"
+            src=".././assets/Clear.gif"
             class="card-img-top"
             alt=""
           />
@@ -60,8 +59,6 @@
       </div>
     </div>
   </div>
-
-  
 </template>
 
 <script>
