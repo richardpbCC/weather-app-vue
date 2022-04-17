ab70e768a9f72e042ed8e3e40243e2f809e83e04
richardpb <rpbeavis@gmail.com>
2022-04-18 00:33:07 +0900

    refactor: remove unused import in main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,3 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
 createApp(App).mount("#app");
-
-// const app = createApp(App);
-// app.use(VueGoogleMaps, {
-//     load: {
-//         key: import.meta.env.VITE_API_KEY,
-//     },
-// }).mount("#app");
